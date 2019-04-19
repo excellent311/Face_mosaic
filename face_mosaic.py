db5467e9e0e65bb8fcc43b6177bce540dd96da07
ximikang <ximikang@outlook.com>
2019-04-19 19:53:58 +0800

    实现图片打码

--- face_mosaic.py
+++ face_mosaic.py
@@ -7,6 +7,21 @@ import logging
 LOG_FORMAT = "************%(asctime)s - %(levelname)s- %(message)s"
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
+def face_detection(image, classifier_path="haarcascade_frontalface_default.xml"):
+    """
+    find all face in input image and return face by coordinate(list)
+
+    input: image, classifier file path (.xml) and have default path
+    output: list coordinate [x1, y1, x2, y2]
+    """
+    classifier_path = os.path.join('data', classifier_path)
+    gray_image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    detector = cv.CascadeClassifier(classifier_path)
+    faces = detector.detectMultiScale(gray_image)
+    for i, face in enumerate(faces):
+        faces[i] = [face[0], face[1], face[0]+face[2], face[1]+ face[3]]
+    return faces
+    
 
 def mosaic(image, coordinate, kernel_size=3):
     """
@@ -17,17 +32,17 @@ def mosaic(image, coordinate, kernel_size=3):
     """
     x1, y1, x2, y2 = coordinate
     image_h, image_w, image_channels = image.shape
-    
+    print(x1, y1, x2, y2)
     num_h = math.floor(abs(y2 - y1) / kernel_size)
     mod_h = abs(y2 - y1) % kernel_size
     num_w = math.floor(abs(x2 - x1) / kernel_size)
     mod_w = abs(x2 - x1) % kernel_size
-
+    print(num_h,mod_h,num_w,mod_w)
     for y in range(num_h):
         for x in range(num_w):
-            image[y*kernel_size:y*kernel_size+kernel_size, x*kernel_size:x*kernel_size+kernel_size,:] = \
+            image[y1+y*kernel_size:y1+y*kernel_size+kernel_size, x1+x*kernel_size:x1+x*kernel_size+kernel_size,:] = \
                 np.mean(
-                    image[y*kernel_size:y*kernel_size+kernel_size, x*kernel_size:x*kernel_size+kernel_size,:],
+                    image[y1+y*kernel_size:y1+y*kernel_size+kernel_size, x1+x*kernel_size:x1+x*kernel_size+kernel_size,:],
                     axis=(0, 1)
                 )
     return image
@@ -47,9 +62,11 @@ def load_image(image_path):
     else:
         print("image file not exists")
 if __name__ == "__main__":
-    image_path = "test.jpg"
+    image_path = "3.jpg"
     image = load_image(image_path)
-    coor = [50,100,500,800]
-    image = mosaic(image, coor, kernel_size=10)
+
+    faces = face_detection(image)
+    for face in faces:
+        image = mosaic(image, face, kernel_size=3)
     cv.imshow('test', image)
     cv.waitKey()
